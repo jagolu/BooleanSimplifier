379b6b6f1ab59900b9b17e4e9935e5a86cd8123a
Javier Gómez Luzón <javo14795@gmail.com>
2024-08-15 21:51:25 +0200

    Add Constants

---
+++ Constants/Constants.cs
@@ -0,0 +1,11 @@
+﻿namespace BooleanSimplifier.Constants
+{
+    public class Constants
+    {
+        public const char LEFT_PARENTESIS = '(';
+        public const char RIGHT_PARENTESIS = ')';
+        public const char OR_OPERATOR = '+';
+        public const char AND_OPERATOR = '*';
+        public const char NEGATION_OPERATOR = '!';
+    }
+}
